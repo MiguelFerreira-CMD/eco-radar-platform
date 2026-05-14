d6a5caa0900a92323805fa2658b8ec1a337d3a30
Miguel Ferreira <freitasmiguel1801@gmail.com>
2026-05-14 16:53:51 -0300

    Site  completo, esperando achar erros...

--- frontend/js/registro.js
+++ frontend/js/registro.js
@@ -21,7 +21,7 @@ form.addEventListener("submit", async (e) => {
         submitBtn.innerText = "Criando conta...";
         submitBtn.disabled = true;
 
-        const response = await fetch("http://localhost:5000/api/auth/register", {
+        const response = await fetch("/api/auth/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
